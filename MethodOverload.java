5165a6ba2c165f825f1fa0e0832c15c710304826
guna-1995 <gunaramanujam95@gmail.com>
2023-09-12 21:16:55 +0530

    methodoover load

---
+++ MethodOverload.java
@@ -0,0 +1,18 @@
+package javaexercise;
+
+public class MethodOverload {
+
+	     public void add (int a, int b) {
+	    	 int c=a+b;
+	    	 System.out.println(c);
+	     }
+	     public void add (float a, float b) {
+	    	 float c=a-b;
+	    	 System.out.println(c);
+	     }
+	     public static void main(String[] args) {
+	}
+
+}
+
+
