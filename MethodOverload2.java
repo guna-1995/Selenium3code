6028ec9889629976409600c51e70ef1a6733695a
guna-1995 <gunaramanujam95@gmail.com>
2023-09-13 18:14:51 +0530

    Method overload program

---
+++ MethodOverload2.java
@@ -0,0 +1,17 @@
+package javaexercise;
+
+public class MethodOverload2 {
+
+	public MethodOverload2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+MethodOverload me2= new MethodOverload() ;
+me2.add(12, 12);
+me2.add(12.5f, 12.5f);
+
+	}
+
+}
